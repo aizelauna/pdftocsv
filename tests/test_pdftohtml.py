153cfe7a73d71125281af5c3175cec32c92b78ae
Aize Launa <aize.launa@gmail.com>
2018-11-25 23:15:30 +0100

    add some tests to xml conversion

--- tests/test_pdftohtml.py
+++ tests/test_pdftohtml.py
@@ -2,19 +2,32 @@ import xml.etree.ElementTree as ET
 from pdftocsv.pdftohtml import convert_to_xml
 
 def test_convert_to_xml():
+    # Convert pdf to xml
     xml = convert_to_xml("tests/assets/trail-fleur-de-lys-2018.pdf")
+
+    # Check that important tags and attributes are correctly converted
     root = ET.fromstring(xml)
 
     pages = root.findall('page')
-
     assert len(pages) == 1
 
-    texts = pages[0].findall('text')
+    fontspecs = pages[0].findall('fontspec')
+    assert len(fontspecs) == 2
+    assert fontspecs[0].attrib['size'] == '12'
+    assert fontspecs[0].attrib['family'] == 'Times'
 
+    texts = pages[0].findall('text')
     assert len(texts) == 265
     assert texts[0].find('b').text == 'Pl.'
     assert texts[0].attrib['top'] == '111'
+    assert texts[0].attrib['left'] == '95'
+    assert texts[0].attrib['width'] == '18'
+    assert texts[0].attrib['height'] == '14'
+    assert texts[0].attrib['font'] == '0'
+
     assert texts[5].text == "1."
     assert texts[5].attrib['top'] == '128'
-    assert texts[238].text == "44. POUX Jean-Paul"
-    assert texts[239].attrib['top'] == '944'
+    assert texts[5].attrib['left'] == '89'
+    assert texts[5].attrib['width'] == '12'
+    assert texts[5].attrib['height'] == '14'
+    assert texts[5].attrib['font'] == '1'
